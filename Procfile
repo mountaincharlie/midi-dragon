fe5037bcb46ad8df495ab3c09358903de48af51f
Charlie <ciharland@hotmail.co.uk>
2022-07-28 11:08:03 +0000

    setup: create midi-dragon project and setup with heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn midi_dragon.wsgi:application
---
+++ midi_dragon/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for midi_dragon project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'midi_dragon.settings')
+
+application = get_asgi_application()
