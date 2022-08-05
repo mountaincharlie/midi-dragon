8e6f6e077c24b1a0022b398a1c3840876717bd6e
Charlie <ciharland@hotmail.co.uk>
2022-08-05 14:33:02 +0000

    feat: create status toggling action methods in SongAdmin

--- songs/admin.py
+++ songs/admin.py
@@ -132,5 +132,52 @@ class SongAdmin(admin.ModelAdmin):
         # 'likes',
     )
 
-    # actions = any methods you want to define below
-    # public, completed and use_as_testimonial toggles
+    actions = ['change_public_status', 'change_completed_status', 'change_testimonial_status']
+
+    def change_public_status(self, request, queryset):
+        """
+        Changes the public status of selected songs
+        Also checks if the completed status is True before it
+        allows the public status to be changed to True (this
+        prevents in-completed songs being able to be made public)
+        """
+        for song in queryset:
+            if song.public:
+                song.public = False
+                song.save()
+            elif song.completed:
+                song.public = True
+                song.save()
+
+    def change_completed_status(self, request, queryset):
+        """
+        Changes the completed status of selected songs
+        Also sets the public status to False if the completed status
+        is being set to False to prevent in-complete songs/songs
+        needing work from being visible to users.
+        """
+        for song in queryset:
+            if song.completed:
+                song.completed = False
+                song.public = False
+                song.save()
+            else:
+                song.completed = True
+                song.save()
+
+    def change_testimonial_status(self, request, queryset):
+        """
+        Changes the testimonial status of selected songs
+        Doesn't need to check that the song is completed since
+        in-complete custom songs can have the 'Use As Testimonial'
+        option selected - they just won't be visible on the
+        Testimonial page until the song is public (which requires
+        it to be complete)
+        """
+        for song in queryset:
+            if song.use_as_testimonial:
+                song.use_as_testimonial = False
+                song.save()
+            else:
+                song.use_as_testimonial = True
+                song.save()
