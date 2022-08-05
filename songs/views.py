1a72621d1bcb497a516b970efafd73b3b2646a5e
Charlie <ciharland@hotmail.co.uk>
2022-08-05 15:35:13 +0000

    feat: create SongsList class based view

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Song
 
-# Create your views here.
+
+class SongsList(generic.ListView):
+    """
+    Class based view inheriting Django's generic.ListView
+    Uses the Song model for sending the searched for public songs to the
+    songs.html template.
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+        Gets all of the songs which are public
+        """
+        public_songs = Song.objects.filter(public=True)
+
+        context = {
+            'songs': public_songs,
+        }
+
+        return render(request, "songs/songs.html", context)
