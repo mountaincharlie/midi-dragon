3a3d07a9f9ba02d86cc266b7d2f3972c32cd5137
Charlie <ciharland@hotmail.co.uk>
2022-08-20 16:23:31 +0000

    feat: add url path for design_custom_song template

--- songs/urls.py
+++ songs/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.SongsList.as_view(), name='songs'),
     path('testimonials/', views.TestimonialsList.as_view(), name='testimonials'),
+    path('design-custom-song/', views.DesignCustomSong.as_view(), name='design_custom_song'),
     path('<slug:slug>/', views.SongDetailsView.as_view(), name='song_details'),
     path('<slug:slug>/likes', views.LikeSong.as_view(), name='song_like'),
     path('<slug:slug>/download/', views.DownloadSong.as_view(), name='download_song'),
