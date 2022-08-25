4b4454f13b7ec5ed8bfa558fe254eb6a9a2882ea
Charlie <ciharland@hotmail.co.uk>
2022-08-25 21:36:03 +0000

    feat: add login_required decorator to design_custom_song url

--- songs/urls.py
+++ songs/urls.py
@@ -2,11 +2,12 @@
 my class based views in views.py """
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.SongsList.as_view(), name='songs'),
     path('testimonials/', views.TestimonialsList.as_view(), name='testimonials'),
-    path('design-custom-song/', views.DesignCustomSong.as_view(), name='design_custom_song'),
+    path('design-custom-song/', login_required(views.DesignCustomSong.as_view()), name='design_custom_song'),
     path('<slug:slug>/', views.SongDetailsView.as_view(), name='song_details'),
     path('<slug:slug>/likes', views.LikeSong.as_view(), name='song_like'),
     path('<slug:slug>/download/', views.DownloadSong.as_view(), name='download_song'),
