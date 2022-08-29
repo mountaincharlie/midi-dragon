b669e2048814a3228992549651933a714f917b7f
Charlie <ciharland@hotmail.co.uk>
2022-08-29 12:52:15 +0000

    feat: create OrderForm for the checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,25 @@
+"""
+Django's forms.py for creating the form I use in views.py
+"""
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """
+    Using the Order model and with only the editable non-auto generated fields
+    avaliable
+    """
+
+    class Meta:
+        model = Order
+        fields = (
+            'full_name',
+            'email',
+        )
+
+        # adding placeholder text with django widgets
+        widgets = {
+            'full_name': forms.TextInput(attrs={'placeholder': 'Full Name'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Email Address'}),
+        }
