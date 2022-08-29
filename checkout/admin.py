c25a9e84ee0ff124c00b8729ac76e1fd58bb423d
Charlie <ciharland@hotmail.co.uk>
2022-08-29 10:44:08 +0000

    feat: create and register OrderAdmin and OrderSongInline classes

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Order, OrderSong
 
-# Register your models here.
+
+class OrderSongInline(admin.TabularInline):
+    """ Tabular inline of the OrderSong table """
+    model = OrderSong
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    """
+
+    readonly_fields = (
+        'order_number',
+        'date',
+        'order_total',
+        # 'original_bag',
+        # 'stripe_pid',
+    )
+
+    list_display = (
+        'order_number',
+        # 'user_profile',
+        'full_name',
+        'date',
+        'order_total',
+    )
+
+    ordering = ('-date',)
+
+    search_fields = (
+        'order_number',
+        # 'user_profile',
+        'full_name',
+        'date',
+    )
+
+    list_filter = (
+        # 'user_profile',
+        'full_name',
+        'email',
+        'date',
+    )
+
+    inlines = [OrderSongInline, ]
