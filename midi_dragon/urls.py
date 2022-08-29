ccfcd02ddf8f48da4b1a1e675e8b1f1fe9cb17d8
Charlie <ciharland@hotmail.co.uk>
2022-08-29 11:41:27 +0000

    setup: add checkout urls paths to the projec level urls

--- midi_dragon/urls.py
+++ midi_dragon/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('songs/', include('songs.urls')),
     path('tracklist/', include('tracklist.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
