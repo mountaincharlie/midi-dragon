1c249bc07857785c996488151c08f3243790d7ba
Charlie <ciharland@hotmail.co.uk>
2022-09-01 23:11:15 +0000

    chore: tidy the handle_payment_intent_succeeded methods docstring and comments

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,9 @@
 """
-Contains a class for handling Stripe's different webhook events which we use to ...FINISH
+Contains a class for handling Stripe's different webhook events
 
-Logic for the StripeWH_Handler adapted for this project, from CI walkthrough - CREDIT - https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/933797d5e14d6c3f072df31adf0ca6f938d02218/checkout/webhook_handler.py
+CREDITS
+Logic for the StripeWH_Handler adapted for this project, from CI walkthrough:
+https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/933797d5e14d6c3f072df31adf0ca6f938d02218/checkout/webhook_handler.py
 """
 import json
 import time
@@ -15,7 +17,7 @@ from songs.models import Song
 
 
 class StripeWH_Handler:
-    """ Handling Stripe webhooks """
+    """ Handling Stripe webhook events """
 
     def __init__(self, request):
         """
@@ -35,57 +37,65 @@ class StripeWH_Handler:
             status=200
         )
 
-
-    # ------methods to handle specific events from Stripe
+    # ------ methods to handle specific events from Stripe
 
     def handle_payment_intent_succeeded(self, event):
         """
         Handles payment_intent.succeeded webhook from Stripe
-        Used each time the user completes the payment process
+        -Used each time the user completes the payment process
         and it is successful
-        If the payment suceeds then a while loop is triggered to
-        search the db to see if the order object exists for the
-        successful playment_intent.
-        The order is searched for using the '_iexact' lookup field on
+        -Gets the intent and extracts the pid (payment intent id) and
+        tracklist data from it
+        -Stores billing and tracklist_total details
+        -If the user is logged in then it gets the Profile to be updated,
+        otherwise the user is an AnonymousUser
+        -Sets the order_exists variable to False by default until the
+        database has been checked
+        -Sets the while loop's 'atempts' to 1
+        -Credit for the while loop logic is the same as linked in this file's
+        docstring
+        -Creates some delay by implementing a while loop to search the db
+        to see if the order object exists for the successful playment_intent.
+        -The order is searched for using the '_iexact' lookup field on
         each of the fields that the Order model requires and giving them
         values from the cached tracklist data.
-        If the order object is not found within a certain amount of time
+        -If the order object is not found within a certain amount of time
         (set by calling the time module's sleep method with a value of 1
-        second) for 5 attempts
-        then an order object is created from the cached data.
+        second) for 5 attempts then an order object is created from the
+        cached data.
+        -If the Order object is found at any point, the while loop is broken
+        out of and the _send_confirmation_email() method is called with the
+        order instance passed into it
+        -The success httpresponse is sent to Stripe with status = 200
+        -If the object is not found in this time, then it is likely that
+        something went wrong with the form submission even though the payment
+        was made successfully, so a Try/Except block is set up to try to create the order instance and then each OrderSong instance is created
+        -A confirmation email is then sent for the created order and a success
+        httpresponse is sent to Stripe.
+        -If an error occurs, the created order is deleted, if it exists, and
+        an error httpresponse is sent to Stripe with status = 500 (causing
+        Stripe to automatically try the request again later)
         """
 
-        print('REQUEST EXISTS?', self.request)
-
-        # getting the intent and extracting the variables we need from it
         intent = event.data.object
-        # pid = payment_intent_id
         pid = intent.id
         tracklist = intent.metadata.tracklist
-        # tracklist_json = intent.metadata.tracklist_json
-        print('TRACKLIST TYPE', type(tracklist))
-        print('TRACKLIST LIST', tracklist)
 
-        # stores billing and tracklist_total details
         billing_details = intent.charges.data[0].billing_details
         tracklist_total = round(intent.charges.data[0].amount / 100, 2)
 
-        # update the user profile
-        profile = None  # so that it still works for anonymous users
+        # WILL BE USED ONCE PROFILES APP IS CREATED
+        profile = None
         username = intent.metadata.username
-
         # checking if the user is authenticated (can use request.user.is_authenticated aswell)
         # if username != 'AnonymousUser':
             # profile = UserProfile.objects.get(user__username=username)
 
-        # ---- checking if the order already exists (was successful)
-        order_exists = False  # set to False by default until found
+        order_exists = False
         attempt = 1
-        # creates a bit of delay using a number of while loop iterations to allow time for the payment to be processed before treating it as failed (CREDIT for CI walkthrough in doctsring)
         while attempt <= 5:
             try:
                 print('getting the order')
-                # finding the order
                 order = Order.objects.get(
                     full_name__iexact=billing_details.name,
                     email__iexact=billing_details.email,
@@ -94,53 +104,39 @@ class StripeWH_Handler:
                     stripe_pid=pid,
                 )
                 order_exists = True
-                # breaking from the loop if found
                 break
-            # if the order hasnt been found yet/doesnt exist
             except Order.DoesNotExist:
-                # incrementing attempts if not found yet
                 attempt += 1
                 time.sleep(1)
 
         if order_exists:
-            print('ORDER EXISTS')  # REMOVE when done
-            # send confirmation email
+            # ADD SENDING REAL EMAILS WITH DJANGO - send confirmation email
             print('SENDING CONFIRMATION EMAIL...')  # REMOVE when done
             # self._send_confirmation_email(order)
-            # http response
+
             return HttpResponse(
                 content=f'Webhook recieved: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
             try:
-                print('CREATING ORDER')
-                # creating the order
                 order = Order.objects.create(
                     full_name=billing_details.name,
                     email=billing_details.email,
-                    # order_total=tracklist_total,
                     original_tracklist=tracklist,
                     stripe_pid=pid,
                 )
                 for song in json.loads(tracklist):
-                    # trying to get the song object from its slug (checking it still exists)
-                    print('TRACKLIST:', tracklist)
-                    print('SONG:', song)
                     existing_song = Song.objects.get(slug=song)
-                    print('EXISTING SONG:', existing_song)
                     order_song = OrderSong(
                             order=order,
                             song=existing_song,
                         )
                     order_song.save()
-                    # CLEAR SESSION DATA?
 
             except Exception as e:
-                # if anything goes wrong, we need to delete order if it exists
                 if order:
                     order.delete()
-                # status 500 response casues Stripe to automatically try again later
                 return HttpResponse(
                     content=f'Webhook recieved: {event["type"]} | ERROR: {e}', status=500)
 
