aa10c4a76ba8b8c770d2c9407bddba67a80e584f
Charlie <ciharland@hotmail.co.uk>
2022-09-01 21:00:28 +0000

    feat: add cache_checkout_data and webhook url paths

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,8 +2,11 @@
 my class based views in views.py """
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.CheckoutView.as_view(), name='checkout'),
     path('order_confirmation/<order_number>', views.OrderConfirmation.as_view(), name='order_confirmation'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
