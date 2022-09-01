0d86f626868c6480923acdef7f0278ce19fc5b91
Charlie <ciharland@hotmail.co.uk>
2022-09-01 22:32:13 +0000

    chore: tidy comments into docstrings with clearer credit references

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,9 +1,13 @@
 """
-Contains a function which listens for webhooks from Stripe ...FINISH
+Contains a function which listens for webhooks from Stripe
 
-Logic for the webhook function adapted for this project, from CI walkthrough's adaptations - CREDIT - https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/933797d5e14d6c3f072df31adf0ca6f938d02218/checkout/webhooks.py
+CREDITS
+-Logic for the webhook function adapted for this project, from CI
+walkthrough's adaptations:
+https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/933797d5e14d6c3f072df31adf0ca6f938d02218/checkout/webhooks.py
 
-Original logic for handling webhooks from the Stripe docs - CREDIT - https://stripe.com/docs/payments/handling-payment-events#create-webhook
+-Original logic for handling webhooks from the Stripe docs:
+https://stripe.com/docs/payments/handling-payment-events#create-webhook
 """
 
 import stripe
@@ -17,50 +21,56 @@ from checkout.webhook_handler import StripeWH_Handler
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """ Listens for webhooks from Stripe """
+    """
+    Listens for webhooks from Stripe.
+    -Sets up wh_secret and api key
+    -Gets the webhook data to verify its signature (sig_header)
+    -Tries to create the Stripe Webhook event with these variables
+    -The ValueError exception is for invalid payloads
+    -The stripe.error.SignatureVerificationError exception is for
+    invalid signiture
+    -The final exception is a general exception handler
+    -The last half of the functions logic is adapted for this project
+    from Code Institute's walkthrough [link in file docstring]
+    -Sets up an instance of the webhook handler with the request
+    -Creates an event map dict to map webhook events to the required
+    handler functions from StripeWH_Handler class
+    -Gets the webhook's type from Stripe
+    -Gets the required handler if it exists, else uses the generic one
+    -Calls the event handler with the event and returns the response
+    to Stripe
+    """
 
-    # Setup wh_secret and api key
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # gets the webhook data to verify its signature (sig_header)
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
-    # try to create the event
     try:
         event = stripe.Webhook.construct_event(
             payload, sig_header, wh_secret
         )
     except ValueError as e:
-        # Invalid payload
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
         return HttpResponse(status=400)
     except Exception as e:
-        # generic exception handler from CI walkthrough CREDIT in docstring
         return HttpResponse(content=e, status=400)
-
 
     # ----- event handling logic from CI walkthrough CREDIT in docstring
 
-    # sets up an instance of the webhook handler with the request
     handler = StripeWH_Handler(request)
 
-    # event map dict to map webhook events to the required handler functions from StripeWH_Handler class
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
         'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
-    # gets the webhook's type from Stripe
     event_type = event['type']
 
-    # gets the required handler if it exists, else uses the generic one
     event_handler = event_map.get(event_type, handler.handle_event)
 
-    # calls the event handler with the event and returns the response to Stripe
     response = event_handler(event)
     return response
