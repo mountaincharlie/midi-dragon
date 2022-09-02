e1b1d959593c41494b75e4d881f554db2c5d4b1f
Charlie <ciharland@hotmail.co.uk>
2022-09-02 00:07:08 +0000

    chore: tidy the docstring comments and credits

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,14 @@
 """
-Views based on the logic from the CI walkthrough view functions and adapted for this project
-CREDIT - CI walkthrough [https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/933797d5e14d6c3f072df31adf0ca6f938d02218/checkout/views.py]
+View functions and classes to handle the checkout app functionality
+
+CREDITS
+Views based on the logic from the CI walkthrough view functions and adapted
+for this project
+[https://github.com/Code-Institute-Solutions/boutique_ado_v1/blob/933797d5e14d6c3f072df31adf0ca6f938d02218/checkout/views.py]
+
+Creating the PaymentIntent using auto payment menthods in python from the
+Stripe docs
+[https://stripe.com/docs/payments/accept-a-payment?platform=web&ui=elements]
 """
 import json
 import stripe
@@ -18,23 +26,24 @@ from .forms import OrderForm
 @require_POST
 def cache_checkout_data(request):
     """
-    function to pass customer information through a stripe payment intent as metadata (creating a cache of the tracklist data for the webhook handling)
-    -gets the payment_intent id
-    -sets up stripe with api key
-    -call the modify method on the PaymentIntent with metadata for
-    username and a json dump of the entire bag
+    Function to pass customer information through a stripe payment intent as
+    metadata (creating a cache of the tracklist data for the webhook handling)
+    -Uses a Try/Except block
+    -Gets the payment_intent id from the first part of the 'client_secret'
+    -Sets up stripe with api key from settings.py variable
+    -Call the modify method on the PaymentIntent with metadata for
+    username and a json dump of the entire tracklist
+    -Returns a status 200 httpresponse
+    -If there are any errors then a message is displayed to the user and a
+    status 400 httpresponse is returned
     """
-    #  tracklist is a list for creating orders, tracklist_json is a string used as the original_tracklist field
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            # 'tracklist_json': json.dumps(request.session.get('tracklist', [])),
-            # 'tracklist': request.session.get('tracklist', []),
             'tracklist': json.dumps(request.session.get('tracklist', [])),
             'username': request.user,
         })
-
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, f'Sorry, your payment could not be processed \n at this time please try again later. \n If the issue persists, please contact midiDRAGON')
@@ -46,48 +55,52 @@ class CheckoutView(View):
     Class based view inheriting Django's View
     Uses the Order model for rendering the OrderForm in the
     checkout.html template.
+    -Sets the stripe_public_key and stripe_secret_key
+    -Contains the get method for displaying the form to the user
+    -Contains the post method for handling the form submission posted
+    from stripe_elements.js which handles the form submission
     """
 
-    # stripe vars
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     def get(self, request, *args, **kwargs):
         """
-        FINISH ...
+        -Gets the tracklist list from the session
+        -Includes a check in case the user tries to manually access the
+        checkout through the url when they dont have anything in their
+        tracklist
+        -If their tracklist is empty then a message is displayed to them
+        and theyre redirected to the pre-made songs browsing page
+        -Sets the current_tracklist from the tracklist_contents() in
+        tracklist/contexts.py
+        -Gets the tracklist_total for the current_tracklist
+        -Gets stripe_total as an integer as Stripe requires this
+        -Sets stripe.api_key as our stripe_secret_key
+        -Creates the PaymentIntent (dictionary of details about the payment)
+        credit in this file's docstring
+        -If the user is logged in, their instance of the Profile model is used
+        to prepopulate the full_name and email fields (they're still editable)
+        otherwise just an empty instance of OrderForm is sent to the template
         """
-        # get tracklist list from the session
+
         tracklist = request.session.get('tracklist', [])
 
-        # in case the user access the checkout page manually in the url
-        # (since the secure checkout button is not avaliable in the tracklist
-        # if the tracklist is empty)
         if not tracklist:
             messages.error(request, 'You cannot checkout since there are currently no items in your tracklist')
             return redirect(reverse('songs'))
 
-        # sets the current_tracklist from the tracklist_contents() in tracklist/
-        # contexts.py
         current_tracklist = tracklist_contents(request)
-        # gets the tracklist_total for the current_tracklist
         tracklist_total = current_tracklist['tracklist_total']
-
-        # stripe needs tracklist_total as an integer
         stripe_total = round(tracklist_total * 100)
-
-        # setting stripe.api_key as our stripe_secret_key
         stripe.api_key = self.stripe_secret_key
 
-        # creating the PaymentIntent (dict of details about the payment)
-        # ------ CREDIT - https://stripe.com/docs/payments/accept-a-payment?platform=web&ui=elements [Using auto payment menthods in python]
         payment_intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # checking if the payment_intent works
-        print('PAYMENT INTENT:', payment_intent)
-
+        # FINISH ONCE PROFILES APP CREATED
         # checking if the user is authenticated and prepoping their data if
         # their profile can be found (USERPROFILE MODEL TO BE ADDED SOON)
         if request.user.is_authenticated:
@@ -114,21 +127,29 @@ class CheckoutView(View):
 
     def post(self, request, *args, **kwargs):
         """
-        -gets the bag from the session
-        -if there is nothing in the bag an error message is displayed and the
-        user is redirected to the products page
-        -stores an instance of the OrderForm
-        -defines the template to use
-        -defines the context dict with the OrderForm to be accessed within
-        the template
-        FINISH ...
+        -Gets the tracklist list from the session or creates an empty list if
+        it doesn't exist
+        -Creates a dictionary of form data as teh instance of the form since
+        its submission is handled in stripe_elmenets.js
+        -Sets order_form = the OrderForm instance
+        -If the form is valid, the order is saved but not committed, the 
+        payment intent id (pid) is taken from the first part of the 
+        'client_secret'
+        -Sets the order's paymentIntent id (pid) and original_tracklist with
+        a json dump of the tracklist from the session
+        -Saves the order
+        -Iterates through the song objects in the tracklist list to create
+        each OrderSong instance, using a Try/Except block to check that the song
+        exists
+        -If it doesn't exist anymore, a message is displayed to the user, the
+        order is deleted and the user is redirected to their Tracklist
+        -If it deos exist, then after creating and saving it, the user is
+        redirected to the order_confirmation page.
+        -If the form is invalid, a message is displayed to the user and the
+        checkout page is reloaded with the data that was posted by the user
         """
-        print('POST METHOD')
-        # get tracklist list from the session
         tracklist = request.session.get('tracklist', [])
 
-        # dict of data from the form (stores an instance of the OrderForm)
-        # creating the form instance since the submit is handled with JS and we only want the full_name and email input values for our form [CREDIT - CI walkthrough]
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -136,24 +157,16 @@ class CheckoutView(View):
 
         order_form = OrderForm(form_data)
 
-        # saving the form if its valid
         if order_form.is_valid():
-            print('ORDER FORM IS VALID')
             order = order_form.save(commit=False)
-            # the bit of client_secret before the '_secret' is the payment intent id
             pid = request.POST.get('client_secret').split('_secret')[0]
 
-            # setting the order's paymentIntent id (pid)
-            # and original_tracklist with a json dump of the tracklist from the session
             order.stripe_pid = pid
             order.original_tracklist = json.dumps(tracklist)
             order.save()
-
-            # iterating through the song objects in the tracklist list
-            # to create each OrderSong instance
+ 
             for song in tracklist:
                 try:
-                    # trying to get the song object from its slug (checking it still exists)
                     existing_song = Song.objects.get(slug=song)
                     order_song = OrderSong(
                             order=order,
@@ -161,8 +174,6 @@ class CheckoutView(View):
                         )
                     order_song.save()
                 except Song.DoesNotExist:
-                    # displaying error msg, deleting the order and redirecting
-                    # to the tracklist
                     messages.error(request, (
                         f'"{song}" was not found in our database.'
                         'Please contact midiDRAGON for assistance.'
@@ -170,37 +181,42 @@ class CheckoutView(View):
                     order.delete()
                     return redirect(reverse('tracklist'))
 
-            # redirecting to the order_confirmation page, passing in the order_number
             return redirect(reverse(
                 'order_confirmation', args=[order.order_number]
                 ))
         else:
-            print('the order form is NOT valid')
-            # error message that the form isnt valid (they're redirected at
-            # the view bottom)
             messages.error(request, (
                 'There was an error processing your order form. \n Please ensure all your details have been correctly filled out.'
             ))
+
+        client_secret = request.POST.get('client_secret')
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': self.stripe_public_key,
+            'client_secret': client_secret,
+        }
+
+        return render(request, 'checkout/checkout.html', context)
 
 
 class OrderConfirmation(View):
     """
     Class based view inheriting Django's View
     view to handle successful checkouts
-    -takes in the request an the order_number for the order just created
-    -gets the order by its number
-    -displays success msg with the order number
-    -delete the bag fro mthe session
-    -set template and context
-    -return render
-    FINISH ...
+    -Contains the get method for displaying the Order overview to the user
     """
     def get(self, request, *args, **kwargs):
         """
-        FINISH ...
+        -Takes in the request and the order_number for the order just created
+        -Gets the order by its number
+        -If the user is logged in then it adds their profile to the order
+        instance
+        -Displays a success message to the user with the order number and the 
+        email that the confirmation will be sent to
+        -Clears the tracklist from the session
+        -Passes the order instance through the context
+        -return renders the order_confirmation page
         """
-
-        # getting the order instance from the order number
         order = get_object_or_404(Order, order_number=self.kwargs['order_number'])
 
         # checking if the user is authenticated before ataching the users profile to the order [when profile app created]
@@ -214,7 +230,6 @@ class OrderConfirmation(View):
         # clearing the session tracklist
         if 'tracklist' in request.session:
             del request.session['tracklist']
-
 
         context = {
             'order': order,
