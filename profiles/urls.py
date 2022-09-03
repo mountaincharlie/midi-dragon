674fe133c18a75a59313e8c23036c3bb1c1e17cb
Charlie <ciharland@hotmail.co.uk>
2022-09-03 02:51:09 +0000

    feat: add url paths to each profile dashboard page

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,13 @@
 """ Django's urls.py for specifying the url paths for each of
 my class based views in views.py """
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
-# from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', views.ProfileDashboard.as_view(), name='dashboard'),
+    path('my-details/', login_required(views.MyDetailsView.as_view()), name='my_details'),
+    path('project-drafts/', login_required(views.ProjectDraftsView.as_view()), name='project_drafts'),
+    path('projects-in-progress/', login_required(views.ProjectsInProgressView.as_view()), name='projects_in_progress'),
+    path('completed-projects/', login_required(views.CompletedProjectsView.as_view()), name='completed_projects'),
+    path('order-history/', login_required(views.OrderHistoryView.as_view()), name='order_history'),
 ]
