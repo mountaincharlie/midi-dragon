3e7fef44c1a448cd99e18a216186e6ba4755cad9
Charlie <ciharland@hotmail.co.uk>
2022-09-04 14:37:13 +0000

    feat: add a check to the index view function to remove songs that users have purchased from their tracklist, on login

--- home/views.py
+++ home/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
+from checkout.models import Order, OrderSong
 
 
 def index(request):
-    """ View for returning the index.html """
+    """
+    View for returning the index.html
+    If the user has just logged in, it checks if songs which may have been
+    added to the tracklist before they logged in, have already been purchased
+    by that user, then they are removed from their tracklist so that they
+    don't accidentally purchase them twice.
+    The check:
+    -gets the tracklist songs from the session (or an empty string if it
+    doesn't exist yet)
+    -gets all of the user's orders
+    -creates an empty list to add all of the songs which have been ordered
+    by the user
+    -for each order, it then gets all of the associated OrderSong objects
+    -for each ordersong in order_songs it then appends the ordersong
+    by its .song.slug to the songs_ordered_by_user list
+    -then for each ordered_song in the songs_ordered_by_user list, if
+    that song slug exists in the tracklist list, then it is removed from
+    the tracklist list
+    """
+
+    if request.user.is_authenticated:
+
+        tracklist = request.session.get('tracklist', [])
+        users_orders = Order.objects.filter(user_profile=request.user.id)
+
+        songs_ordered_by_user = []
+        for order in users_orders:
+            order_songs = OrderSong.objects.filter(order=order)
+            for ordersong in order_songs:
+                songs_ordered_by_user.append(ordersong.song.slug)
+
+        for ordered_song in songs_ordered_by_user:
+            if ordered_song in tracklist:
+                tracklist.remove(ordered_song)
 
     return render(request, 'home/index.html')
