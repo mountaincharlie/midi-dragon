3099577ce59f9158c0fde14c4892be40f8729b2f
Charlie <ciharland@hotmail.co.uk>
2022-09-05 12:01:59 +0000

    refactor: remove unused field from Song model and move post_save to seperate signals file

---
+++ songs/__init__.py
@@ -0,0 +1,2 @@
+# tell django the default config class for the app
+default_app_config = 'songs.apps.SongsConfig'
--- songs/apps.py
+++ songs/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SongsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'songs'
+
+    def ready(self):
+        import songs.signals
