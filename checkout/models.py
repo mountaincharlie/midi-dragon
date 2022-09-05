a3b169d3a06b2bbacfc0e8cf4b8d8b9c0e75fd87
Charlie <ciharland@hotmail.co.uk>
2022-09-05 11:35:43 +0000

    refactor: move checkout app post_save and post_delete signals into a seperate signals.py file

--- checkout/models.py
+++ checkout/models.py
@@ -7,8 +7,8 @@ Order model is based on and adapted from Code Institute's walkthrough
 
 import uuid
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+# from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
 from django.db import models
 from songs.models import Song
 
@@ -142,42 +142,3 @@ class OrderSong(models.Model):
     def __str__(self):
         """ Returns the song name and order_number it belongs to """
         return f'{self.song.name} on order {self.order.order_number}'
-
-
-# MOVE SIGNALS INTO A SEPERATE signals.py file
-
-@receiver(post_save, sender=OrderSong)
-def update_on_save(sender, instance, created, **kwargs):
-    """
-    Handles functions from the OrderSong models post_save event 
-    sender => OrderSong
-    instance => the instance of the OrderSong that sent it
-    created = > django boolean for if this is a new instance or being updated
-    Reciver decorator to pass info about whats happening here
-    For the instance of OrderSong, it calls the Order model's update_total()
-    method, passing in the price of the song.
-    """
-    song_price = instance.song.price
-
-    print('UPDATE ON SAVE CALLED')
-
-    instance.order.update_total(song_price)
-
-
-@receiver(post_delete, sender=OrderSong)
-def update_on_delete(sender, instance, **kwargs):
-    """
-    Handles functions from the OrderSong models post_delete event 
-    sender => OrderSong
-    instance => the instance of the OrderSong that sent it
-    Reciver decorator to pass info about whats happening here
-    For the instance of OrderSong, it calls the Order model's update_total()
-    method, passing in the negative of price of the song (so that when its
-    added to the order_total in the update_total() method, it will actually
-    be subtracting from the total).
-    """
-    song_price = (instance.song.price) * -1
-
-    print('UPDATE ON DELETE CALLED')
-
-    instance.order.update_total(song_price)
---
+++ checkout/signals.py
@@ -0,0 +1,40 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderSong
+
+
+@receiver(post_save, sender=OrderSong)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Handles functions from the OrderSong models post_save event 
+    sender => OrderSong
+    instance => the instance of the OrderSong that sent it
+    created = > django boolean for if this is a new instance or being updated
+    Reciver decorator to pass info about whats happening here
+    For the instance of OrderSong, it calls the Order model's update_total()
+    method, passing in the price of the song.
+    """
+    song_price = instance.song.price
+
+    print('UPDATE ON SAVE CALLED')
+
+    instance.order.update_total(song_price)
+
+
+@receiver(post_delete, sender=OrderSong)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Handles functions from the OrderSong models post_delete event 
+    sender => OrderSong
+    instance => the instance of the OrderSong that sent it
+    Reciver decorator to pass info about whats happening here
+    For the instance of OrderSong, it calls the Order model's update_total()
+    method, passing in the negative of price of the song (so that when its
+    added to the order_total in the update_total() method, it will actually
+    be subtracting from the total).
+    """
+    song_price = (instance.song.price) * -1
+
+    print('UPDATE ON DELETE CALLED')
+
+    instance.order.update_total(song_price)
