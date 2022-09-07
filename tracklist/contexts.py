61705658e9dbc3332a8b945db850bbd9dc6b5c06
Charlie <ciharland@hotmail.co.uk>
2022-09-07 07:18:00 +0000

    style/fix: run all tracklist app files through validators and document

--- tracklist/contexts.py
+++ tracklist/contexts.py
@@ -12,19 +12,25 @@ def tracklist_contents(request):
     Context processor
     Returns context dictionary of data for the tracklist
     The function is avaliable accross all the templates
-    """
 
-    # setting up vars
+    If updating_tracklist = 'true' the toast_success html snippet
+    will display the tracklist overview in the message, else it will
+    only show the message text (e.g. when logging in/out or creating
+    a new song design project)
+    Stores in the updating_tracklist variable wether the tracklist
+    is being updated in the session
+    Resets updating_tracklist in the SESSION so that it will be set to
+    'false' by default unless the AddToTracklist() or
+    RemoveFromTracklist() views set it to 'true' again
+    """
     # list containing song object
     tracklist_songs = []
     tracklist_total = 0
     # get the var if it exists or assign empty list
     tracklist = request.session.get('tracklist', [])
-    # storing in the updating_tracklist variable, wether the tracklist is being updated in the session
-    updating_tracklist = request.session.get('updating_tracklist', 'false')
-    # if 'true' the toast_success html snippet will display the tracklist overview in the message, else it will only show the message text (e.g. when logging in/out or creating a new song design project)
 
-    # resetting updating_tracklist in the SESSION so that it will be set to 'false' by default unless the AddToTracklist() or RemoveFromTracklist() views set it to 'true' again
+    updating_tracklist = request.session.get('updating_tracklist', 'false')
+
     request.session['updating_tracklist'] = 'false'
 
     # song_slug is the key and the song object is its value
@@ -37,14 +43,11 @@ def tracklist_contents(request):
             tracklist_total += song.price
             # add the song to the tracklist_songs list
             tracklist_songs.append(song)
-        # ELSE ? some note to be triggered in the tracklist that one of their songs was deleted or not found
 
     context = {
         'tracklist_songs': tracklist_songs,
         'tracklist_total': tracklist_total,
         'updating_tracklist': updating_tracklist,
-
-        # 'grand_total': grand_total,  # only need if you use discounts later
     }
 
     return context
--- tracklist/views.py
+++ tracklist/views.py
@@ -1,3 +1,4 @@
+""" views.py for the tracklist app """
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
 from django.views import View
@@ -5,13 +6,14 @@ from songs.models import Song
 
 
 def view_tracklist(request):
-    """ View to return the tracklist page """
-
-    # stopping the superuser being able to access the tracklist since they shouldnt be able to buy their own songs
+    """
+    View to return the tracklist page
+    -Stops the superuser being able to access the tracklist
+    since they shouldnt be able to buy their own songs
+    """
     if request.user.is_superuser:
-        messages.info(request, "Redirecting Admin to Site Management page.")
+        messages.info(request, "Redirecting Admin to Site Management.")
         return redirect(reverse('all_songs'))
-
 
     return render(request, 'tracklist/tracklist.html')
 
@@ -25,34 +27,43 @@ class AddToTracklist(View):
         """
         View to add a song to the tracklist and redirect to the
         same song_details url or song search results
-        """
+        -gets the song by its pk inorder to include its name in
+        messages
+        -gets url to redirect to
+        -stores the tracklist in a http session so that the content
+        of the tracklist is not overwritten or lost while browsing
+        the site until the browser is closed
+        -gets the var if it exists or assign it an empty list
+        -the song's slug is used as its key in the tracklist list
+        -additional checks if its already in their tracklist it
+        displays message and doesnt add to tracklist
 
-        # getting the song by its pk inorder to include its name in messages
+        -sets the updating_tracklist var to 'true' in the session
+        so that in toast_success.html it creates the message with
+        the tracklist view displayed
+        """
         song = get_object_or_404(Song, slug=self.kwargs['slug'])
-        #  gets url to redirect to
         redirect_url = request.POST.get('redirect_url')
 
-        # storing the tracklist in a http session so that the contents of the tracklist is not overwritten or lost while browsing the site until the browser is closed
-        # get the var if it exists or assign it an empty list
         tracklist = request.session.get('tracklist', [])
 
-        # the song's slug is used as its key in the tracklist list
         song_slug = song.slug
 
-        # additional check if its already in their tracklist it displays message and doesnt add to tracklist
         if song_slug in tracklist:
-            messages.error(request, f"You already have {song.name} in your tracklist")
-            print(f"You already have {song.name} in your tracklist")
+            messages.error(
+                request,
+                f"You already have {song.name} in your tracklist"
+            )
         else:
             # add the song_slug to the tracklist list
             tracklist.append(song_slug)
-            print(f'{song.name} added to tracklist')
-            messages.success(request, f"{song.name} has been added to your tracklist")
+            messages.success(
+                request,
+                f"{song.name} has been added to your tracklist"
+            )
 
         # updating the tracklist var in the session dict
         request.session['tracklist'] = tracklist
-
-        # setting the updating_tracklist var to 'true' in the session so that in toast_success.html it creates the message with the tracklist view displayed
         request.session['updating_tracklist'] = 'true'
 
         # to check the session thing works
@@ -65,42 +76,52 @@ class AddToTracklist(View):
 class RemoveFromTracklist(View):
     """
     Class based view inheriting Django's View
-    Contains the post method for 
+    Contains the post method for removing songs
+    from the Tracklist
     """
     def post(self, request, *args, **kwargs):
         """
         View to remove a song from the tracklist and redirect to the
         same tracklist page
-        """
+        -gets the song by its pk inorder to include its name in
+        messages
+        -gets url to redirect to
+        -the song's slug is used as its key in the tracklist list
+        -gets tracklist list from the session
+        -checks if the song is in the list and removing it
 
-        # getting the song by its pk inorder to include its name in messages
+        -updates the tracklist var in the session list
+        -sets the updating_tracklist var to 'true' in the session
+        so that in toast_success.html it creates the message with
+        the tracklist view displayed
+        -
+        """
         song = get_object_or_404(Song, slug=self.kwargs['slug'])
-        #  gets url to redirect to
         redirect_url = request.POST.get('redirect_url')
-        # the song's slug is used as its key in the tracklist list
         song_slug = song.slug
 
-        # get tracklist list from the session
         tracklist = request.session.get('tracklist')
 
-        # checking if the song is in the list and removing it
         if song_slug in tracklist:
             tracklist.remove(song_slug)
             if len(tracklist) == 0:
-                messages.success(request, f"{song.name} was removed from your tracklist. \n Your Tracklist is now empty.")
+                messages.success(
+                    request,
+                    f"{song.name} was removed from your tracklist. \n Your Tracklist is now empty."
+                )
             else:
-                messages.success(request, f"{song.name} was removed from your tracklist.")
+                messages.success(
+                    request,
+                    f"{song.name} was removed from your tracklist."
+                )
         else:
-            messages.error(request, f"There was an error while trying to remove {song.name} from your tracklist.")
+            messages.error(
+                request,
+                f"There was an error while trying to remove {song.name} from your tracklist."
+            )
 
-        # updating the tracklist var in the session list
         request.session['tracklist'] = tracklist
-
-        # setting the updating_tracklist var to 'true' in the session so that in toast_success.html it creates the message with the tracklist view displayed
         request.session['updating_tracklist'] = 'true'
-
-        # to check the session thing works
-        print(request.session['tracklist'])
 
         # calls the hidden redirect_url input
         return redirect(redirect_url)
